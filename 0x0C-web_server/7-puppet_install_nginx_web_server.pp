695e42163fb8f6563db4554fad93a611f30977c1
Saint Alaye <96530933+SaintAlaye@users.noreply.github.com>
2023-10-09 00:39:35 +0100

    Update 7-puppet_install_nginx_web_server.pp

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,24 +1,38 @@
-# Setup New Ubuntu server with nginx
+#!/usr/bin/env bash
+# Configure Nginx server to give error 404 page
+apt-get update
+apt-get -y install nginx
+ufw allow 'Nginx HTTP'
+chmod -R 755 /var/www
+echo 'Hello World' > /var/www/html/index.html
 
-exec { 'update system':
-        command => '/usr/bin/apt-get update',
+new_config=\
+"server {
+        listen 80 default_server;
+        listen [::]:80 default_server;
+               root /var/www/html;
+        index index.html index.htm index.nginx-debian.html;
+        server_name _;
+        location / {
+                try_files \$uri \$uri/ =404;
+        }
+        error_page 404 /404.html;
+        location  /404.html {
+            internal;
+        }
+        
+        if (\$request_filename ~ redirect_me){
+            rewrite ^ https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;
+        }
 }
+"
+echo "Ceci n'est pas une page" > /var/www/html/404.html
 
-package { 'nginx':
-	ensure => 'installed',
-	require => Exec['update system']
-}
+echo "$new_config" > /etc/nginx/sites-available/default
 
-file {'/var/www/html/index.html':
-	content => 'Hello World!'
-}
-
-exec {'redirect_me':
-	command => 'sed -i "24i\	rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;" /etc/nginx/sites-available/default',
-	provider => 'shell'
-}
-
-service {'nginx':
-	ensure => running,
-	require => Package['nginx']
-}
+if [ "$(pgrep nginx)" -le 0 ];
+then
+    service nginx start
+else
+    service nginx restart
+fi;
